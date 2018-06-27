663ebb01da74b1217f2564a8e05cfcb41a9fbfd1
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-06-27 14:49:08 +0100

    - create index controller - register service worker

---
+++ scripts/IndexController.js
@@ -0,0 +1,20 @@
+// export the IndexController using requirejs
+define(function () {
+  return IndexController;
+});
+
+class IndexController {
+  constructor() {
+    this.registerServiceWorker()
+  }
+
+  registerServiceWorker() {
+    if (!navigator.serviceWorker) return;
+    navigator.serviceWorker.register('service-worker.js').then((reg) => {
+      console.log('service worker registered')
+    }).catch((error) => {
+      console.log('service worker registration failed', error)
+    });
+  }
+}
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,4 @@
-requirejs(["converter"], function () {
+// use requirejs to include the converter and index controller file
+requirejs(["converter", "IndexController"], (undefined, IndexController) => {
+  new IndexController()
 });
